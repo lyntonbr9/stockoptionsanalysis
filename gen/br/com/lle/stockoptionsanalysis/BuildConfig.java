b636801d76eb2505ddc1d60c46e0993fedf61488
lyntonbr <lyntonbr@localhost>
2015-01-01 12:51:32 -0200

    - troca do eclipse adt para o android studio

--- gen/br/com/lle/stockoptionsanalysis/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package br.com.lle.stockoptionsanalysis;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/br/com/lle/stockoptionsanalysis/mobile/Constants.java
+++
@@ -1,9 +0,0 @@
-package br.com.lle.stockoptionsanalysis.mobile;
-
-import com.google.gson.Gson;
-
-public class Constants {
-	
-	public static Gson gson = new Gson();
-
-}
